43793a705ebbfdf1005d361d63f2b955812f1e2c
BradenEverson <bradeneverson@gmail.com>
2024-11-07 11:23:57 -0600

    Input reading example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,22 @@
 //! Example driver program that blinks an LED
 
-use jetson::{gpio::valid_pins::Pin7, Gpio};
+use jetson::{
+    gpio::valid_pins::{Pin5, Pin7},
+    Gpio,
+};
 use std::{thread, time::Duration};
 
 fn main() {
     let gpio = Gpio::new().expect("Initialize GPIO");
     let mut led = gpio.get_output(Pin7).expect("Set GPIO pin 7 to output");
+    let button = gpio.get_input(Pin5).expect("Get input button");
 
-    for _ in 0..500 {
-        led.toggle().expect("Toggle");
-        thread::sleep(Duration::from_millis(500));
+    loop {
+        if button.read().expect("Set High") {
+            led.set_high().unwrap();
+        } else {
+            led.set_low().expect("Set Low");
+        }
+        thread::sleep(Duration::from_millis(50));
     }
 }
